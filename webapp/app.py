2c6c590d001f69e1b0b181d79c7a90c3a6967803
aepuertol <andrespuertolara@gmail.com>
2023-10-04 18:14:17 -0500

    webapp working

--- webapp/app.py
+++ webapp/app.py
@@ -12,12 +12,34 @@ app.layout=html.Div([
 
     dcc.Upload(id='uploader',children=['Drag and drop'],style={'width':'50%','borderStyle':'dashed'}),
     html.Div(id='output-div'),
-        
-    html.Div(id='video-container'),
-    
+    html.Div([
+        html.Div(id='men')
+
+    ]),
+  
+    html.Table([  # Create an HTML table
+        # Header row
+        html.Tr([
+            html.Th("Original", style={'border': '1px solid gray', 'text-align': 'center'}),  # Header cell 1
+            html.Th("Translated", style={'border': '1px solid gray', 'text-align': 'center'})   # Header cell 2
+        ]),
+
+        # Data row
+        html.Tr([
+            html.Td(
+                dcc.Loading(id='video-1',children=[html.Div(id='video-container')],type='circle'), 
+                style={'border': '1px solid gray'}
+            ),  
+            html.Td(
+               dcc.Loading(id='video-2',children=[ html.Div(id='video-translated')],type='circle'), 
+               style={'border': '1px solid gray'}
+            )   # Data cell 2
+        ])
+    ]),
+
     html.Div(id='translate-container-btn',children=[html.Button('translate',id='translate-button',n_clicks=0)]),
     
-    html.Div(id='video-translated'),
+    
     dcc.Store(id='filename-temp')
     
    
@@ -39,7 +61,7 @@ def process_video_content(contents,filename):
                 videofile.write(base64.b64decode(contents.split(',')[1]))
             
             #video_embedded=dash_player.DashPlayer(id='video-player',url=video_path,controls=True)
-            video_embedded=html.Video(id='video-player',src=video_path,controls=True)
+            video_embedded=html.Video(id='video-player',src=video_path,controls=True,height='480',width='640')
             
             #button=html.Button('translate',id='translate-button',n_clicks=0)
             #return f'Uploaded {filename}',button,{'video_path':video_path}
@@ -58,15 +80,17 @@ def translate_video(n_clicks,filename_dict):
     if ctx.triggered_id=='translate-button':
         video_name=filename_dict['video_path']
         data = {
-            "srclang": "es",
-            "dstlang": "it",
+            "srclang": "pt",
+            "dstlang": "es",
             "filename":video_name
         }
         resp=requests.post('http://localhost:5000/translate',json=data)
-        return resp['response']
+        #print(resp.json())
+        video_translated=html.Video(id='video-translated',src=resp.json()['response'],controls=True,height='480',width='640')
+        return video_translated
     else:
         return ''
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,dev_tools_hot_reload=False)
